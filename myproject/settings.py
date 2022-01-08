a5284c2dafa944aab39ccbc0b092507900a9ec6e
adityarj-pazuzu <aditya.jambhalikar001@gmail.com>
2022-01-08 14:23:46 +0530

    added secret_key for csrf

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,5 +12,6 @@ DB_HOST = getenv('DB_HOST')
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+ DB_USERNAME + ':%s'%quote(DB_PASSWORD) + '@' + DB_HOST + '/' + DATABASE_NAME
 if DB_USERNAME is None:'''
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
